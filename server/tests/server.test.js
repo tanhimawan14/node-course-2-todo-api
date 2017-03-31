71d08b9fcb117819acec7ac1826cc7e30f7af917
tanhimawan14 <himawan.tan.b@gmail.com>
2017-03-31 15:52:22 +0700

    Add PATCH route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,7 @@
+const _ = require('lodash');
 const expect = require('expect');
 const request = require('supertest');
+
 const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
