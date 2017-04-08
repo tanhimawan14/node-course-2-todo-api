c25cf19c8a1d6be01c08e2cac2da1561f22231a4
tanhimawan14 <himawan.tan.b@gmail.com>
2017-04-08 14:49:23 +0700

    Add POST users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -50,7 +50,27 @@ UserSchema.methods.generateAuthToken = function () {
   return user.save().then(() => {
     return token;
   });
+};
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // Use bcrypt.compare to compare password and user.password
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
 };
 
 UserSchema.statics.findByToken = function (token) {
